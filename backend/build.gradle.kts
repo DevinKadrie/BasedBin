d2f0aa4436fd0108a9ac784900be56761806fa5a
Shane <shane.everitt.murphy@gmail.com>
2024-10-05 18:48:16 -0700

    Chore: use gradle version catalog

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    kotlin("jvm") version "2.0.10"
-    id("io.ktor.plugin") version "2.3.12"
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.10"
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlinx.serialization)
+    alias(libs.plugins.ktor)
 }
 
 group = "com.github.devinkadrie.basedbin"
@@ -26,20 +26,15 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm:2.3.12")
-    implementation("io.ktor:ktor-server-resources-jvm:2.3.12")
-    implementation("io.ktor:ktor-server-cors-jvm:2.3.12")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:2.3.12")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:2.3.12")
-    implementation("io.ktor:ktor-server-netty-jvm:2.3.12")
-    implementation("io.ktor:ktor-server-config-yaml:2.3.12")
-    implementation("com.fasterxml.uuid:java-uuid-generator:5.1.0")
-    implementation("aws.sdk.kotlin:s3:1.3.23")
+    implementation(libs.bundles.ktor.core)
+    implementation(libs.bundles.ktor.plugins)
 
-    implementation("org.postgresql:postgresql:42.7.2")
-    implementation("com.h2database:h2:2.1.214")
-    implementation("ch.qos.logback:logback-classic:1.4.14")
+    implementation(libs.uuid)
+    implementation(libs.h2)
+    implementation(libs.s3)
+    implementation(libs.postgres)
+    implementation(libs.logback)
 
-    testImplementation("io.ktor:ktor-server-test-host-jvm:2.3.12")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:2.0.10")
+    testImplementation(libs.kotlin.test)
+    testImplementation(libs.bundles.ktor.test)
 }
